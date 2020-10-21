ee189edc0b60a3c538a3e24947d8c74579c35b93
Manojkumar B B <63506938+manoj4617@users.noreply.github.com>
2020-10-21 19:15:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,48 @@
 const { static } = require('express');
 const express = require('express');
 const { v4: uuidv4 } = require('uuid');
+const { ExpressPeerServer } = require('peer');
+
 
 const app = express();
-
 const server = require('http').Server(app);
 const io = require('socket.io')(server)
+const peerServer = ExpressPeerServer(server, {
+    debug:true
+});
+app.use('/peerjs', peerServer);
 
+//view engine
 app.set('view engine','ejs');
 
+//for static files
 app.use(express.static('public'));
 
+//routes
 app.get('/',(req,res)=>{
     res.redirect(`/${uuidv4()}`);
 });
 
 app.get('/:index',(req,res)=>{
     res.render('index',{roomID: req.params.index});
+});
+
+//making socket.io connection
+io.on('connection', socket => {
+    socket.on('join-room',(roomID, userid)=>{
+        socket.join(roomID);
+        socket.to(roomID).broadcast.emit('user-connected', userid);
+        //receiving messages
+        socket.on('message',(message)=>{
+            console.log(message)
+            //sending the received message to everyone in the same room
+            io.to(roomID).emit('createMessage',message);
+        });
+        socket.on('disconnect',()=>{
+            socket.to(roomID).broadcast.emit('user-disconnected',userid)
+        })
+    });
+    
 });
 
 server.listen(4000);
